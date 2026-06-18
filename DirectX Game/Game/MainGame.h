fee27117bdd17a842eb7fd3fdf0a86a772e00e28
SADIO-M <megan_florence_sadio@dlsu.edu.ph>
2026-06-18 17:59:48 +0800

    Update MainGame.h

--- DirectX Game/Game/MainGame.h
+++ DirectX Game/Game/MainGame.h
@@ -16,6 +16,6 @@ class MainGame : public szas::Game
 		//LIST OF GAME OBJECTS
 		//szas::AGameObject* m_objects[9]{};
 		szas::AGameObject* m_player{};
-		szas::f32 m_rotation{0.0f}, m_scale{1.0f};
+		szas::f32 m_rotation{}, m_scale{};
 };
 
