00bf11817e67c6c05c4c4f30ac65a99eb4b9a263
SADIO-M <megan_florence_sadio@dlsu.edu.ph>
2026-06-23 02:29:38 +0800

    Update Player.cpp

--- DirectX Game/Game/Player/Player.cpp
+++ DirectX Game/Game/Player/Player.cpp
@@ -73,17 +73,17 @@ void szas::Player::OnUpdate(f32 deltaTime)
 		}
 	}
 
-	//auto forwardDir = GetTransform().Forward() * GetForwardModifier();
-	//auto rightDir = GetTransform().Right() * GetRightModifier();
-	//auto directionSum = forwardDir + rightDir;
-	//
-	//if (szas::Vec3::LengthSquared(directionSum) > 0.001f)
-	//{
-	//	auto direction = szas::Vec3::Normalize(directionSum);
-	//	position = position + direction * GetSpeedModifier() * deltaTime;
-	//}
-	//
-	//GetTransform().SetPosition(position);
+	auto forwardDir = GetTransform().Forward() * GetForwardModifier();
+	auto rightDir = GetTransform().Right() * GetRightModifier();
+	auto directionSum = forwardDir + rightDir;
+	
+	if (szas::Vec3::LengthSquared(directionSum) > 0.001f)
+	{
+		auto direction = szas::Vec3::Normalize(directionSum);
+		position = position + direction * GetSpeedModifier() * deltaTime;
+	}
+	
+	GetTransform().SetPosition(position);
 }
 
 szas::Player::~Player()
